bc92c4958367cd73fe874152924733d9381c9807
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-13 19:36:19 +0100

    Forgot to commit earlier, though a large portion of my time was spent trying to figure out the structure, Niall very kindly helped overcome a major bug that had me stumped. Managed to sort the logic for my code out and get a win condition, which Jenny very kindly helped me with.

---
+++ main.js
@@ -0,0 +1,79 @@
+// get a grid on the screen
+
+//when you click on each grid tile, make x and then o appear
+
+//after each player has made a move, check to see if someone has won.
+
+//if winner - display winners message.
+
+var player1 = 1;
+var $box1 = $('#1')
+var $box2 = $('#2')
+var $box3 = $('#3')
+var $box4 = $('#4')
+var $box5 = $('#5')
+var $box6 = $('#6')
+var $box7 = $('#7')
+var $box8 = $('#8')
+var $box9 = $('#9')
+
+function turns(){
+
+
+	$('.square').click(function (event){
+
+		if (player1 == 1) {
+			$(this).addClass('x');
+
+			player1 = 0;
+			wins('x');
+
+		} else {
+
+			$(this).addClass('o');
+
+			player1 = 1;
+			wins('o')
+		}
+	});
+}
+turns()
+
+
+function wins(player){
+	if($box1.hasClass(player) && $box2.hasClass(player) && $box3.hasClass(player)){
+		console.log('win')
+	}
+
+	if ($box4.hasClass(player) && $box5.hasClass(player) && $box6.hasClass(player)){
+		console.log('win')
+	}
+
+	if ($box7.hasClass(player) && $box8.hasClass(player) && $box9.hasClass(player)){
+		console.log('win')
+	}
+
+	if($box1.hasClass(player) && $box4.hasClass(player) && $box7.hasClass(player)){
+		console.log('win')
+	}
+
+	if ($box2.hasClass(player) && $box5.hasClass(player) && $box8.hasClass(player)){
+		console.log('win')
+	}	
+
+	if ($box3.hasClass(player) && $box6.hasClass(player) && $box9.hasClass(player)){
+		console.log('win')
+	}
+
+	if ($box1.hasClass(player) && $box5.hasClass(player) && $box9.hasClass(player)){
+		console.log('win')
+	}
+
+	if ($box3.hasClass(player) && $box5.hasClass(player) && $box7.hasClass(player)){
+		console.log('win')
+	}
+}
+
+
+
+
